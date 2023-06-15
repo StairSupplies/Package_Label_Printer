57c6d7e4dd9ae2513ea2152b4ff956aeade7d890
Matt_Graber_of_Viewrail <matthew.graber@viewrail.com>
2023-06-15 12:54:44 -0400

    modified:   app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,15 @@ from flask import Flask
 from flask_socketio import SocketIO, send
 
 
-
 app = Flask(__name__, static_url_path='/static')
-
 
 #adds a security key found in the config.py file for protection
 #app.config.from_object(Config)
 #CORS(app)
 #app.config['SECRET_KEY'] = 'you-cannot-guess'
-socketio = SocketIO(app)
+
+if __name__ == "__main__":
+    socketio = SocketIO(app)
 
 
 
